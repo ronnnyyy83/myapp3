19a7fb38caba9ecae99d4d76dedea4207d95aa9f
Roni Bardavid <roni@Ronis-MacBook-Pro.local>
2023-10-09 23:48:17 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const elementRef = React.useRef<HTMLInputElement>(null);
+  const escFunction = (e:any) =>{
+    const robotDiv = elementRef.current;//document.getElementById('robot');
+    if (e.key === "w") {
+      if(robotDiv && robotDiv.getBoundingClientRect().left >= 58)
+      //console.log(robotDiv.getBoundingClientRect().left)
+        robotDiv.style.left = (robotDiv.getBoundingClientRect().left - 54) + "px";
+    } else if (e.key === "e") {
+      if(robotDiv && robotDiv.getBoundingClientRect().left <= 482)
+      robotDiv.style.left = (robotDiv.getBoundingClientRect().left + 54) + "px";
+    } else if (e.key === "n") {
+      if(robotDiv && robotDiv.getBoundingClientRect().top >= 58)
+      robotDiv.style.top = (robotDiv.getBoundingClientRect().top - 54) + "px";
+    } else if (e.key === "s") {
+      if(robotDiv && robotDiv.getBoundingClientRect().top <= 482)
+      robotDiv.style.top = (robotDiv.getBoundingClientRect().top + 54) + "px";
+    }
+  }
+
+  useEffect(()=>{
+    //const divElement = elementRef.current;
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    }
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div id="robot" className='imageContainer' ref={elementRef}>
+          <img src={logo} alt=""/>
+        </div>
+        {Array.from(Array(10), (e, r) => 
+          <>
+            {Array.from(Array(10), (e, c) => 
+              <div key={r.toString() + "-" + c.toString()} id={r.toString() + "-" + c.toString()} className="tile"></div>
+            )}
+          </>
+        )}
+      
     </div>
   );
 }
